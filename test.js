4494fa7471184556aac6ff74fef07d578283467c
Girish Ramakrishnan <girish@forwardbias.in>
2014-11-22 18:23:28 -0800

    API change: lastMileHandler -> lastMile

--- test.js
+++ test.js
@@ -12,14 +12,8 @@ var connectLastMile = require('./lib/index.js'),
     HttpSuccess = connectLastMile.HttpSuccess;
 
 describe('Exports', function () {
-    it('has exported individual handlers', function () {
-        expect(connectLastMile.successHandler).to.be.a(Function);
-        expect(connectLastMile.clientErrorHandler).to.be.a(Function);
-        expect(connectLastMile.serverErrorHandler).to.be.a(Function);
-    });
-
     it('has exported composited handler', function () {
-        expect(connectLastMile.lastMileHandler).to.be.an(Array);
+        expect(connectLastMile.lastMile).to.be.an(Array);
     });
 
     it('has exported success class', function () {
